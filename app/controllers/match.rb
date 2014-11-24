fe961a3e580640745001097b2e5212bf0888b83a
Beatrix "Betsy" Carroll <beatrixcarroll@gmail.com>
2014-11-24 13:58:02 -0500

    made it so that you can only see matches who are the gender you are looking for, and who are looking for your gender

--- app/controllers/match.rb
+++ app/controllers/match.rb
@@ -1,6 +1,6 @@
 get '/match/all' do
   protected!
   @user = User.find(session[:user_id])
-  @matches = User.where("location = ? AND id != ?", @user.location, @user.id).select { |match| match.stds.name == @user.stds.name }
+  @matches = User.where("location = ? AND id != ?", @user.location, @user.id).select { |match| match.stds.name == @user.stds.name && match.looking_for == @user.gender && @user.looking_for == match.gender}
   erb :'match/all'
 end
